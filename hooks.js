46c4d065bb171bd4e47acd4ad56fec216194adff
Moussa Alkaly BADJI <moussabadji079@gmail.com>
2022-08-20 12:57:38 +0000

    add the function to get data in a specific field

--- hooks.js
+++ hooks.js
@@ -1,16 +1,15 @@
 import { collection, onSnapshot } from "firebase/firestore";
 import { useEffect, useState } from "react";
 
-export function useDocs(db, collectionName) {
+function useDocs(db, collectionName) {
   const [data, setData] = useState([]);
-  const [dataLoading, setDataLoading] = useState(true)
-  const [numberOfData, setNumberOfData] = useState(0)
+  const [dataLoading, setDataLoading] = useState(true);
+  const [numberOfData, setNumberOfData] = useState(0);
 
   useEffect(() => {
     const getDocs = async () => {
-      
       onSnapshot(collection(db, collectionName), (querySnapshot) => {
-        setNumberOfData(querySnapshot.size)
+        setNumberOfData(querySnapshot.size);
         const allData = [];
         querySnapshot.forEach((doc) => {
           allData.push({ ...doc.data(), id: doc.id });
@@ -21,6 +20,41 @@ export function useDocs(db, collectionName) {
     };
     getDocs();
   }, []);
-  
+
   return { data, numberOfData, dataLoading };
 }
+
+const useWhereDocs = (
+  db,
+  collectionName,
+  whereToLookInTheCollection,
+  whereToLookValue
+) => {
+  const [data, setData] = useState([]);
+  const [dataLoading, setDataLoading] = useState(true);
+  const [numberOfData, setNumberOfData] = useState(0);
+  
+  useEffect(() => {
+    const getWhereDocs = async () => {
+      if (!whereToLook) return;
+      const q = query(
+        collection(db, collectionName),
+        where(whereToLookInTheCollection, "==", whereToLookValue)
+      );
+      onSnapshot(q, (querySnapshot) => {
+        setNumberOfData(querySnapshot.size);
+        const allData = [];
+        querySnapshot.forEach((doc) => {
+          allData.push({ ...doc.data(), id: doc.id });
+          setData([...allData]);
+        });
+        setDataLoading(false);
+      });
+    };
+    getWhereDocs();
+  }, [whereToLookValue]);
+
+  return { data, numberOfData, dataLoading };
+};
+
+export { useDocs, useWhereDocs };
