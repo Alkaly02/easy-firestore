5e23c0ecee919a444561f188bd113ad3323795e8
Moussa Alkaly BADJI <moussabadji079@gmail.com>
2022-08-21 16:08:00 +0000

    add useFile to handle images

--- hooks.js
+++ hooks.js
@@ -1,6 +1,8 @@
 import { collection, onSnapshot, query, where } from "firebase/firestore";
 import { useEffect, useState } from "react";
+import { getDownloadURL, ref, uploadBytesResumable } from 'firebase/storage';
 
+// get All Docs
 function useDocs(db, collectionName) {
   const [data, setData] = useState([]);
   const [dataLoading, setDataLoading] = useState(true);
@@ -13,8 +15,8 @@ function useDocs(db, collectionName) {
         const allData = [];
         querySnapshot.forEach((doc) => {
           allData.push({ ...doc.data(), id: doc.id });
-          setData([...allData]);
         });
+        setData([...allData]);
         setDataLoading(false);
       });
     };
@@ -24,6 +26,7 @@ function useDocs(db, collectionName) {
   return { data, numberOfData, dataLoading };
 }
 
+// get Docs With Where()
 const useWhereDocs = (
   db,
   collectionName,
@@ -46,8 +49,8 @@ const useWhereDocs = (
         const allData = [];
         querySnapshot.forEach((doc) => {
           allData.push({ ...doc.data(), id: doc.id });
-          setData([...allData]);
         });
+        setData([...allData]);
         setDataLoading(false);
       });
     };
@@ -57,4 +60,39 @@ const useWhereDocs = (
   return { data, numberOfData, dataLoading };
 };
 
-export { useDocs, useWhereDocs };
+// get image url
+const useFile = (storage, file) => {
+    const [percentage, setPercentage] = useState(null);
+    const [imgUrl, setImgUrl] = useState('')
+    useEffect(() => {
+        const uploadFile = () => {
+            const name = new Date().getTime() + file.name;
+
+            const storageRef = ref(storage, name);
+            const uploadTask = uploadBytesResumable(storageRef, file);
+
+            uploadTask.on(
+                "state_changed",
+                (snapshot) => {
+                    const progress =
+                        (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+                    setPercentage(progress);
+                },
+                (error) => {
+                    console.log(error);
+                },
+                () => {
+                    getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => {
+                        setImgUrl(downloadURL)
+                    });
+                }
+            );
+        };
+        file && uploadFile();
+    }, [file]);
+
+    return {imgUrl, setImgUrl, percentage}
+}
+
+
+export { useDocs, useWhereDocs, useFile };
